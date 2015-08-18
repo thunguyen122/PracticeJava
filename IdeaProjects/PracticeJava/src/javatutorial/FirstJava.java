7d71867d8b026445973a57b6778cf0f24ce3ffb6
Thunguyen <thunguyen122@gmail.com>
2015-08-18 18:51:15 -0400

    first commit practice Java

---
+++ IdeaProjects/PracticeJava/src/javatutorial/FirstJava.java
@@ -0,0 +1,23 @@
+package javatutorial;
+
+/**
+ * Created by MeoU on 8/18/2015.
+ */
+public class FirstJava {
+    public static void main (String [] args) {
+        // lets create variables
+        int number1 = 1;
+        int number2 = 2;
+        int number3 = number1 + number2;
+        String name = "Thu";//String has to be within ""
+        double decimal = 32.45;
+        System.out.println(number3);
+        System.out.println(name);
+        System.out.println(decimal);
+        // other way of outputting
+        System.out.println("When we add "+number1+" and "+number2+", we get "+number3);
+        // other basic mathematical function: add "+", subtract "-", multiply "*", device "/"
+        System.out.println(5-3);
+
+        }
+    }
